52e61a9ea495d146ed4f0c876359bd5be96aeac1
NotDiego7 <Lopez.d7@outlook.com>
2023-09-26 21:28:35 -0600

    Progress

--- Projects/Test/test.cpp
+++ Projects/Test/test.cpp
@@ -1,14 +1,31 @@
+// #include <iostream>
+
+// int main() {
+//     int fav_num;
+
+//     std::cout << "Enter your favorite number between 1 and 100: ";
+    
+//     std::cin >> fav_num;
+
+//     std::cout << "That's my favorite number too." << std::endl;
+
+
+//     return 0;
+// }
+
 #include <iostream>
+using namespace std;
 
 int main() {
-    int fav_num;
+    cout << "Enter the width or height. " << endl;
+    int first_num {};
+    cin >> first_num;
 
-    std::cout << "Enter your favorite number between 1 and 100: ";
-    
-    std::cin >> fav_num;
+    cout << "Now enter the second integer. " << endl;
+    int second_num {};
+    cin >> second_num;
 
-    std::cout << "That's my favorite number too." << std::endl;
+    int result {first_num * second_num};
 
-
-    return 0;
+    cout << "Result: " << result << endl;
 }
