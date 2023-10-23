f696f25fd32a12e1a6ed451360f756e4815ab7de
NotDiego7 <Lopez.d7@outlook.com>
2023-10-23 09:50:53 -0600

    Progress

--- Projects/Arrays and Vectors/main.cpp
+++ Projects/Arrays and Vectors/main.cpp
@@ -9,15 +9,38 @@
 //     cout << month[29] << endl;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     char string [] {"World Ipsum Lacra, bonu"};
+
+//     cout << string << endl;
+    
+//     string[1] = 'j';
+
+//     cout << string << endl;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int table_arr [2][2];
+
+//     table_arr[0][0] = 95;
+//     table_arr[1][1] = 50;
+
+//     cout << table_arr[0][0] << table_arr[1][1] << endl;
+// }
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    char string [] {"World"};
+    vector <string> word {"Lame."};
 
-    cout << string << endl;
-    
-    string[1] = 'j';
-
-    cout << string << endl;
+    cout << word << endl;
 }
